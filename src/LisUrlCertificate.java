e4f3bb5a8ca7a2e6f5ebd22b04a82b9cfce1c41d
oleg_vlasenko <vlasenko.inbox@gmail.com>
2019-10-17 10:06:21 +0300

    first commit

---
+++ src/LisUrlCertificate.java
@@ -0,0 +1,40 @@
+import java.net.URL;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateExpiredException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class LisUrlCertificate {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        LisUrlCertificate tester = new LisUrlCertificate();
+        try {
+            tester.testConnectionTo("https://sso.luxoft.com/adfs/services/trust");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testConnectionTo(String aURL) throws Exception {
+        URL destinationURL = new URL(aURL);
+        HttpsURLConnection conn = (HttpsURLConnection) destinationURL
+                .openConnection();
+        conn.connect();
+        Certificate[] certs = conn.getServerCertificates();
+        for (Certificate cert : certs) {
+            System.out.println("Certificate is: " + cert);
+            if(cert instanceof X509Certificate) {
+                try {
+                    ( (X509Certificate) cert).checkValidity();
+                    System.out.println("Certificate is active for current date");
+                } catch(CertificateExpiredException cee) {
+                    System.out.println("Certificate is expired");
+                }
+            }
+        }
+    }
+}
